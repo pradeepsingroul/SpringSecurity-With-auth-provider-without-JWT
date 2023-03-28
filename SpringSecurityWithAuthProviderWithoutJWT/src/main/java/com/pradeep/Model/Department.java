13361088b6a4102294bc6a6110149df9054a2123
predeep kumar singroul <prasingroul4899@gmail.com>
2023-03-28 16:21:01 +0530

    Models

---
+++ SpringSecurityWithAuthProviderWithoutJWT/src/main/java/com/pradeep/Model/Department.java
@@ -0,0 +1,23 @@
+package com.pradeep.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    @Id
+    private String deptName;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "department")
+    private List<Employee> employees = new ArrayList<>();
+
+}
---
+++ SpringSecurityWithAuthProviderWithoutJWT/src/main/java/com/pradeep/Model/Employee.java
@@ -0,0 +1,32 @@
+package com.pradeep.Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    private int emplId;
+
+    private String empName;
+    @Column(unique = true)
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private String address;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Department department;
+
+
+
+
+
+
+}
