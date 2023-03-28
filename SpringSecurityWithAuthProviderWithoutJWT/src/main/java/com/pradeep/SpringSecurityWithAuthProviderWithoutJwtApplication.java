aa9d40671a626296cf027c339debcedff8fbff54
predeep kumar singroul <prasingroul4899@gmail.com>
2023-03-28 16:22:46 +0530

    applicaion

---
+++ SpringSecurityWithAuthProviderWithoutJWT/src/main/java/com/pradeep/SpringSecurityWithAuthProviderWithoutJwtApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithAuthProviderWithoutJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithAuthProviderWithoutJwtApplication.class, args);
+	}
+
+}
